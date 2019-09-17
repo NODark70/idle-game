ff55f7f47a2862e92a787a8ac0dbd4f45aca2e9b
Kofi Darko <nodark70@harrisonburg.k12.va.us>
2019-09-17 13:33:22 -0400

    Update game.js

--- game.js
+++ game.js
@@ -2,13 +2,20 @@ var score = 0;
 
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress); // Make a button
+createButton('Kofi', btnPress); // Make a button
+changeTitle('Clicker Game');
+createButton('bam', btnPress); // Make a button
 changeTitle('Clicker Game');
 
 setInterval(btnPress, 1000);
 
 function btnPress() {
-  score++;
+  score--;
   postToPage(score);
-}
 
+
+
+
+
+
+}
