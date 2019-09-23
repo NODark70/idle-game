e38340e86e792fcd44d45b13e83b36862d75b9c0
Nana Kofi Darko <nodark70@iMac720-462.local>
2019-09-23 14:09:32 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,10 +1,17 @@
 var score = 1; // Create a variable to store the score in
 
-var title = new Title('The Game of Life'); // Create a title element
-var button = new Button('Go', btnPress);   // Create a button element
+var title = new Title('In the Year 2099'); // Create a title element
+var button = new Button('Be Born', btnPress);   // Create a button element
 var button = new Button('Rules', btnPress);   // Create a button element
 var button = new Button('High Scores', btnPress);   // Create a button element
+let mySection = new Section();
+var rockButton = new Button('Collect rocks');
+var stoneButton = new Button('Collect stones');
 
+var buttonSection = new Section();
+
+buttonSection.add(rockButton);
+buttonSection.add(stoneButton);
 
 var scr = new Text(score);                    // Create a text element
 
@@ -22,12 +29,12 @@ function btnPress() {
 }
 
 
-var button = new Button('Grow', btnPress);   // Create a button element
-var button = new Button('Eat Milk', btnPress);   // Create a button element
-var button = new Button('Eat Applesauce', btnPress);   // Create a button element
-var button = new Button('Life Points', btnPress);   // Create a button element
-var button = new Button('High Scores', btnPress);   // Create a button element
-var button = new Button('High Scores', btnPress);   // Create a button element
-var button = new Button('High Scores', btnPress);   // Create a button element
-var button = new Button('High Scores', btnPress);   // Create a button element
-var button = new Button('High Scores', btnPress);   // Create a button element
+//var button = new Button('Grow', btnPress);   // Create a button element
+//var button = new Button('Eat Milk', btnPress);   // Create a button element
+//var button = new Button('Eat Applesauce', btnPress);   // Create a button element
+//var button = new Button('Life Points', btnPress);   // Create a button element
+//var button = new Button('Store', btnPress);   // Create a button element
+//var button = new Button('High Scores', btnPress);   // Create a button element
+//var button = new Button('High Scores', btnPress);   // Create a button element
+//var button = new Button('High Scores', btnPress);   // Create a button element
+//var button = new Button('High Scores', btnPress);   // Create a button element
